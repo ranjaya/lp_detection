d1b0c4146607e0d988c223fed9e5db1ac02a0f95
ranjaya <ricky.arif13@gmail.com>
2018-05-10 07:49:16 +0000

    ResNet|dense4sigmoid_lr0.001

--- ResNet50_ImgDetect.py
+++ ResNet50_ImgDetect.py
@@ -1,14 +1,17 @@
 from keras.utils.io_utils import HDF5Matrix
 from keras.applications import ResNet50
+from keras.layers import Dense, Flatten
+from keras.models import Model
 from keras.optimizers import Adam
 
+from keras.callbacks import ModelCheckpoint, CSVLogger, TensorBoard
 from keras import backend as K
 
 def lp_train_generator():
     i=0
-    img_data = HDF5Matrix('C:/Users/Ricky/PycharmProjects/untitled/lp_train.h5','images')
-    lbl_data = HDF5Matrix('C:/Users/Ricky/PycharmProjects/untitled/lp_train.h5','labels')
-    size = 1582
+    img_data = HDF5Matrix('lp_train.h5','images')
+    lbl_data = HDF5Matrix('lp_train.h5','labels')
+    size = 1266
 
     while 1:
         img_single = img_data[i % size].reshape((1,320,240,3))
@@ -21,9 +24,9 @@ def lp_train_generator():
 
 def lp_valid_generator():
     i=0
-    img_data = HDF5Matrix('C:/Users/Ricky/PycharmProjects/untitled/lp_valid.h5','images')
-    lbl_data = HDF5Matrix('C:/Users/Ricky/PycharmProjects/untitled/lp_valid.h5','labels')
-    size = 339
+    img_data = HDF5Matrix('lp_valid.h5','images')
+    lbl_data = HDF5Matrix('lp_valid.h5','labels')
+    size = 160
 
     while 1:
         img_single = img_data[i % size].reshape((1,320,240,3))
@@ -36,9 +39,9 @@ def lp_valid_generator():
 
 def lp_test_generator():
     i=0
-    img_data = HDF5Matrix('C:/Users/Ricky/PycharmProjects/untitled/lp_test.h5','images')
-    lbl_data = HDF5Matrix('C:/Users/Ricky/PycharmProjects/untitled/lp_test.h5','labels')
-    size = 339
+    img_data = HDF5Matrix('lp_test.h5','images')
+    lbl_data = HDF5Matrix('lp_test.h5','labels')
+    size = 160
 
     while 1:
         img_single = img_data[i % size].reshape((1,320,240,3))
@@ -64,8 +67,6 @@ def iou(y_true,y_pred):
     w2 = y_true[0][2]
     h2 = y_true[0][3]
 
-    print('x1 = ', x1)
-
     #convert the value of each variable from normalize form to integer
     min = K.cast(0, dtype='float32')
     max_xw = K.cast(320, dtype='float32')
@@ -78,10 +79,10 @@ def iou(y_true,y_pred):
 
     #calculation for IoU
     and_x1, and_y1 = K.maximum(x1,x2), K.maximum(y1,y2)
-    and_x2, and_y2 = K.minimum(x1,x2), K.minimum(y1,y2)
+    and_x2, and_y2 = K.minimum(x1+w1,x2+w2), K.minimum(y1+h1,y2+h2)
 
-    and_w = and_x2 - and_x1
-    and_h = and_y2 - and_y1
+    and_w = K.maximum(min, and_x2 - and_x1)
+    and_h = K.maximum(min, and_y2 - and_y1)
 
     # if K.less_equal(and_w, min) and K.less_equal(and_h, min):
     #     return K.cast(0, dtype='float32')
@@ -90,7 +91,7 @@ def iou(y_true,y_pred):
     area2 = w2*h2
     or_area = area1 + area2 - and_area
 
-    return and_area / or_area
+    return K.maximum(min, and_area / or_area)
 
 
 if __name__ == '__main__':
@@ -101,17 +102,54 @@ if __name__ == '__main__':
     nb_epoch = 100
     init_epoch = 0
 
-    batch_size = 16
+    batch_size = 32
 
     # lr = 1e-03
     lr = 1e-04
 
-    train_size = 1582
-    valid_size = 339
-    test_size = 339
+    train_size = 1266
+    valid_size = 160
+    test_size = 160
 
 
     ###model vgg
     base_model = ResNet50(input_shape=(320,240,3),include_top=False,classes=num_classes, pooling='avg')
     print(base_model.summary())
-    # x = base_model.get_layer('block5_pool').output
+    x = base_model.get_layer('avg_pool').output
+    x = Flatten()(x)
+    fc1 = Dense(4, activation='sigmoid', name='fcnew1')(x)
+
+    #create custom ResNet
+    custom_model = Model(inputs=base_model.input, outputs=fc1)
+    print(custom_model.summary())
+
+    #freeze body's model
+    for layer in custom_model.layers[:-1]:
+        layer.trainable = False
+
+    for n in custom_model.layers:
+        print(n, n.trainable)
+
+    print(custom_model.summary())
+
+    ###optimizer
+    opt = Adam(lr=lr, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+    custom_model.compile(optimizer=opt, loss='mse', metrics=[iou])
+
+    checkpoint = ModelCheckpoint("ResNet50_best_weight.hdf5", monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+    csv_logger = CSVLogger('Resnet_license_plate.csv', append=False)
+    tensorboard = TensorBoard(log_dir='./tf-logs-licenseplate-ResNet')
+    callback_list = [checkpoint, csv_logger, tensorboard]
+
+    result = custom_model.fit_generator(lp_train_generator(),
+            train_size // batch_size,
+            1000,
+            # init_epoch = 0,
+            validation_data=lp_valid_generator(),
+            validation_steps=valid_size//batch_size,
+            shuffle=True,
+            callbacks=callback_list
+            )
+
+    accuracy = custom_model.evaluate_generator(lp_test_generator(),steps=test_size//batch_size)
+
