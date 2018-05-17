b02ae20e7ccd800b96ac8be6f4bf9fc560df8e7f
ranjaya <ricky.arif13@gmail.com>
2018-05-17 05:16:23 +0000

    8192fcrelu_4fcsigmoid_lr0.01

--- ResNet50_ImgDetect.py
+++ ResNet50_ImgDetect.py
@@ -1,6 +1,6 @@
-### 17.05.18 | dense 4096 relu
+### 17.05.18 | dense 8192 relu
 ###            dense 4 sigmoid
-### lr = 0.000001
+### lr = 0.01
 ###
 ###
 ### 10.05.18 | 13.00 custom model: + 2 layer [dense 2048 relu] + bias & kernel reg 0.001 
@@ -115,11 +115,11 @@ if __name__ == '__main__':
 
     batch_size = 32
 
-    #lr = 1e-02
+    lr = 1e-02
     #lr = 1e-03
     #lr = 1e-04
     #lr = 1e-05
-    lr = 1e-06
+    #lr = 1e-06
 
     train_size = 1266
     valid_size = 160
@@ -133,7 +133,7 @@ if __name__ == '__main__':
     x = Flatten()(x)
     #fc1 = Dense(4096, activation='relu', name='fcnew1',kernel_regularizer=regularizers.l2(0.001), bias_regularizer=regularizers.l2(0.001))(x)
     #dropout = Dropout(0.1)
-    fc1 = Dense(4096, activation='relu', name='fcnew1')(x)
+    fc1 = Dense(8192, activation='relu', name='fcnew1')(x)
     fc2 = Dense(4, activation='sigmoid', name='fcnew3')(fc1)
 
     #create custom ResNet
